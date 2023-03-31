f6ebfb2b28487bd3397a47c00f5aa599578b233a
Number5IsAlive <james.s.mason@hotmail.com>
2023-03-31 13:30:45 +0100

    7 - Player Input System

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 1.0f;
+
+    private PlayerControls playerControls = null;
+    private Vector2 movement = Vector2.zero;
+    private Rigidbody2D rb = null;
+
+    private void Awake()
+    {
+        playerControls = new PlayerControls();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    void Update()
+    {
+        PlayerInput();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void PlayerInput()
+    {
+        movement = playerControls.Movement.Move.ReadValue<Vector2>();
+    }
+
+    private void Move()
+    {
+        rb.MovePosition(rb.position + (movement * moveSpeed * Time.fixedDeltaTime));
+    }
+}
