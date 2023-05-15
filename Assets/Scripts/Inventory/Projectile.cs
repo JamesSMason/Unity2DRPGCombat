abfa64a68ea45ae57790d66b1b29c72000d8c0ad
Number5IsAlive <james.s.mason@hotmail.com>
2023-05-15 10:47:37 +0100

    55 Arrow Projectile

--- Assets/Scripts/Inventory/Projectile.cs
+++ Assets/Scripts/Inventory/Projectile.cs
@@ -3,15 +3,51 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 22f;
+    [SerializeField] private GameObject particleOnHitPrefabVFX = null;
+
+    private WeaponInfo weaponInfo = null;
+    private Vector3 startPosition;
+
+    private void Start()
+    {
+        startPosition = transform.position;
+    }
 
     void Update()
     {
         MoveProjectile();
+        DetectFireDistance();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        EnemyHealth enemyHealth = collision.GetComponent<EnemyHealth>();
+        Indestructible indestructible = collision.GetComponent<Indestructible>();
+
+        if (!collision.isTrigger && (enemyHealth || indestructible))
+        {
+            enemyHealth?.TakeDamage(weaponInfo.weaponDamage);
+            Instantiate(particleOnHitPrefabVFX, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+
+    public void UpdateWeaponInfo(WeaponInfo weaponInfo)
+    {
+        this.weaponInfo = weaponInfo;
     }
 
     private void MoveProjectile()
     {
         Vector3 moveDir = Vector3.right * moveSpeed * Time.deltaTime;
         transform.Translate(moveDir);
+    }
+
+    private void DetectFireDistance()
+    {
+        if (Vector3.Distance(transform.position, startPosition) > weaponInfo.weaponRange)
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Scripts/Inventory/WeaponInfo.cs
+++ Assets/Scripts/Inventory/WeaponInfo.cs
@@ -5,4 +5,6 @@ public class WeaponInfo : ScriptableObject
 {
     public GameObject weaponPrefab = null;
     public float weaponCooldown = 3.0f;
+    public int weaponDamage = 0;
+    public float weaponRange = 0;
 }
